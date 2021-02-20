ef58acc5e669e416b60205fb324075df6e5c0c5a
kyon-zono <zono0412@gmail.com>
2021-02-20 17:12:25 +0900

    フェードイン演出実装

--- javascript/script.js
+++ javascript/script.js
@@ -11,6 +11,15 @@ $(function() {
   });
 
   /* ふわぁっ */
-  
+  $(window).scroll(function() {
+    $('.fadein').each(function() {
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 200) {
+        $(this).addClass('scrollin');
+      }
+    });
+  });
 
 }); 
