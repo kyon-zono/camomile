f5d2b47b2c872fd2ec589fb278330cb32f586981
kyon-zono <zono0412@gmail.com>
2021-02-27 02:33:54 +0900

    box add h3 及び 垂直方向のセンタリング

--- javascript/script.js
+++ javascript/script.js
@@ -10,6 +10,38 @@ $(function() {
         }
   });
 
+  /* test-box */
+  
+  
+    /* boxタイトル(h3)の高さを取得 */
+    var h3Height = $('h3').outerHeight();
+    /* boxタイトル(h3)の位置を枠線上に配置 */
+    var boxTitlePosition = h3Height / 2 * -1 - 2; /* 後ろの -1 はboxのborder分を挿れるため */
+    $('.box-content').css('top', boxTitlePosition);
+    /* box-text を上下中央 */
+    var boxText = h3Height/ 2;
+    $('.box-text').css('margin-top', boxText);
+
+    /* position: relative;に高さを与える */
+      /* introduction */
+      var boxHeight = $("#introduction").find('.box-content').outerHeight();
+      $('#introduction').css('height', boxHeight);
+      /* support */
+      var boxHeight = $("#support").find('.box-content').outerHeight();
+      $('#support').css('height', boxHeight);
+      /* about */
+      var boxHeight = $("#about").find('.box-content').outerHeight();
+      $('#about').css('height', boxHeight);
+      /* sns */
+      var boxHeight = $("#sns").find('.box-content').outerHeight();
+      $('#sns').css('height', boxHeight);
+      /* faq */
+      var boxHeight = $("#faq").find('.box-content').outerHeight();
+      $('#faq').css('height', boxHeight);
+      
+  /* ここまで */
+  
+
   /* フェードイン */
   $(window).scroll(function() {
     $('.fadein').each(function() {
