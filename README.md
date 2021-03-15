218e341d03983b17be9114679d66b11684a0408e
kyon-zono <zono0412@gmail.com>
2021-03-15 10:48:29 +0900

    コード整頓&最終チェック

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
 # camomile
-
-# sns-items 
-* snsセクションのみbox内の上下余白をあけるため
-.sns-icons {
-}
-
-# check-code
-* paddinngやmarginを確認するコード
-box-items {
-  background-color: crimson; /* check用 */
-}
-
--- javascript/script.js
+++ javascript/script.js
@@ -19,14 +19,9 @@ $(function() {
   });
 
   /* box */
-
-    /* boxタイトル(h3)の高さを取得 */
     var h3Height = $('h3').outerHeight();
-    /* boxタイトル(h3)の位置を枠線上に配置する数値を代入 */
     var boxTitlePosition = h3Height / 2 * -1 - 2; /* -1 はboxのborder分を計算に含めるため */
-    /* sectionタイトルをborder上にする */
     $('.box-title').css('top', boxTitlePosition);
-    /* ボックスの中身: h3底辺〜box底辺で上下中央寄せする */
     var boxContentTop = h3Height / 2 - 2;
     $('.box-content').css('margin-top', boxContentTop);
   
